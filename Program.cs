45fe20aced5a304d50a5e57be93bf947c0ad4c9c
E1230 <emaurobel1230@hotmail.com>
2023-07-04 06:44:30 -0500

    DB Connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Empresa_Productos;
 using Empresa_Productos.Services;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<Context>(builder.Configuration.GetConnectionString("cnDB"));
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ISupplierService, SupplierService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ISaleService, SaleService>();
 
 var app = builder.Build();
+app.MapGet("/", async ([FromServices] Context dbContext) =>
+{
+  dbContext.Database.EnsureCreated();
+  return Results.Ok();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
